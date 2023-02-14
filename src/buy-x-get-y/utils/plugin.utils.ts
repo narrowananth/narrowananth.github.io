56065b07296132b479a8c5a10b0a975b37d5eaf9
Ananth S <narrowananth@gmail.com>
2023-02-14 16:17:21 +0530

    fix: update the bundle file

--- src/buy-x-get-y/utils/plugin.utils.ts
+++ src/buy-x-get-y/utils/plugin.utils.ts
@@ -1,18 +1,15 @@
 import { resetLineItemAmount, removeExistingDiscount } from "./threshold.utils"
 
 export const buildInputData = (configSchema: object | any, lineItems: Array<any>): object => {
-	const { buyOfferType, getOfferType } = configSchema
+	const { getOfferType } = configSchema
 
-	if (buyOfferType === "threshold" || buyOfferType === "quantity") {
-		lineItems = getOfferType !== "product" ? resetLineItemAmount(lineItems) : lineItems
+	lineItems = getOfferType !== "product" ? resetLineItemAmount(lineItems) : lineItems
 
-		const getRemovedProductList = getOfferType === "product" ? removeExistingDiscount(lineItems) : undefined
+	const getRemovedProductList = getOfferType === "product" ? removeExistingDiscount(lineItems) : undefined
 
-		const config = { ...configSchema, lineItems, getRemovedProductList }
+	const config = { ...configSchema, lineItems, getRemovedProductList }
 
-		return config
-	}
-	return { ...configSchema, lineItems }
+	return config
 }
 
 export const setKeyInFilterProduct = (product: Array<any>): any => {
