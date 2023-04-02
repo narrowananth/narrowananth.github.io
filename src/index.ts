0f157e60464cc7afe94c1db0c39353414687039f
Ananth S <narrowananth@gmail.com>
2023-04-02 20:23:23 +0530

    add

--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,15 @@ export const flow = (appContext: AppContext, configSchema: ConfigSchema[]): stri
 
 		const { offerCategory } = schema
 
-		removeUnitPriceLineItem.push({
-			offerApplied,
-			output: [],
-			getRemovedProductList,
-			displayText: "",
-			totalCartValue: 0,
-			schema: {}
-		})
+		if (!offerApplied)
+			removeUnitPriceLineItem.push({
+				offerApplied,
+				output: [],
+				getRemovedProductList,
+				displayText: "",
+				totalCartValue: 0,
+				schema
+			})
 
 		if (offerCategory === "automaticOffers" && offerApplied) return totalCartValue > 0
 
